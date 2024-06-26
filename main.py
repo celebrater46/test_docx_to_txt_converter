ba621ba9b6933fc0293a65f4380d7a19fc4e8c21
celebrater46 <tkgm45nbchk@gmail.com>
2024-06-26 14:00:37 +0900

    test succeeded

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import docx
+
 def say_hello():
     print("Hello world!!!!!!!!!")
 
 say_hello()
+
+def open_docx():
+    # f = open("files/test.docx")
+    # lines = f.readlines()
+    # print(lines)
+    # f.close()
+
+    document = docx.Document("files/test.docx")
+    # # for paragraph in word.paragraphs:
+    for paragraph in document.paragraphs:
+        print(paragraph.text)
+
+open_docx()
